de24e19b965135886765784da3281d5cc51e640b
suryaSPS <suryasps20@gmail.com>
2024-10-31 01:36:15 +0530

    Time: 20 ms (23.27%), Space: 55.3 MB (80.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> map = new HashSet<>();
-
-        for(int i = 0;i<nums.length;i++){
-            if(map.contains(nums[i])){
-                return true;
-            }
-            map.add(nums[i]);
+        Arrays.sort(nums);
+        for(int i = 1;i<=nums.length -1;i++){
+            if(nums[i] == nums[i-1]) return true;
         }
 
         return false;
-        
     }
 }
