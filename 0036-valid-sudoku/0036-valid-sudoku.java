86ff14716df8a92a546985ea66c505ace4b9103b
suryaSPS <suryasps20@gmail.com>
2024-10-31 11:40:25 +0530

    Time: 2 ms (79.26%), Space: 44.6 MB (34.3%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,15 +1,35 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set present = new HashSet();
-        for(int i = 0;i<9;i++){
-            for(int j = 0;j<9;j++){
-                char num = board[i][j];
-                if(num != '.'){
-                    if(!present.add(num + "r" + i) || !present.add(num + "c" + j) || !present.add(num + "b" + i/3 + "-" + j/3))
-                    return false;
-                }
-            }
-        }
-        return true;
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] col = new HashSet[9];
+        HashSet<Character>[] box = new HashSet[9];
+    
+
+    for(int i = 0;i<9;i++){
+        row[i] = new HashSet<>();
+        col[i] = new HashSet<>();
+        box[i] = new HashSet<>();
     }
+
+    for(int r = 0;r<9;r++){
+        for(int c = 0;c<9;c++){
+            if(board[r][c] == '.'){
+                continue;
+            }
+
+            char value = board[r][c];
+            int ind = (r/3) * 3 + (c/3);
+
+            if(row[r].contains(value) || col[c].contains(value) || box[ind].contains(value)){
+                return false;
+            }
+
+            row[r].add(value);
+            col[c].add(value);
+            box[ind].add(value);
+        }
+    }
+
+    return true;
+}
 }
